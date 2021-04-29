17eb68be7a6e958325851713267a7eb74201a3cf
analuizasb <analuizasb2@gmail.com>
2021-04-29 15:44:20 -0300

    corrigido findByName

--- controllers/gradeController.js
+++ controllers/gradeController.js
@@ -26,7 +26,7 @@ const findAll = async (req, res) => {
 
   try {
     logger.info(`GET /grade`);
-    const allGrades = await gradeModel.find({ name: condition });
+    const allGrades = await gradeModel.find({ name: name });
     res.send(allGrades);
   } catch (error) {
     res
